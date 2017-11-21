364169ce6aebbe9aff1c1e0a949ffb3a0b93c946
pierre <pal@odoo.com>
2017-11-21 13:31:51 +0100

    [IMP] Add routes to controllers + create website templates.

--- sport_club_manager/controllers/controllers.py
+++ sport_club_manager/controllers/controllers.py
@@ -18,13 +18,14 @@ from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 #         return http.request.render('module_test.object', {
 #             'object': obj
 #         })
+_URL_ROOT = '/sport'
 
-class Sport(AuthSignupHome): # http.Controller):
+class SportClubManager(AuthSignupHome): # http.Controller):
     # @http.route('/sport/sport/', auth='public', website=True)
     # def index(self, **kw):
     #     return "Hello, world"
 
-    @http.route('/sport/period/', auth='public', website=True)
+    @http.route('%s/period/' % _URL_ROOT, auth='public', website=True)
     def period(self, **kw):
         periods = http.request.env['period'].search([])
         # periods = http.request.env['period'].search(['|', ('active', '=', True), ('active', '=', False)])
@@ -32,26 +33,43 @@ class Sport(AuthSignupHome): # http.Controller):
             'periods': periods,
         })
 
-    @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
-    def web_auth_signup(self, *args, **kw):
-        return super(Sport, self).web_auth_signup(*args, **kw)
+    # @http.route('%s/login/' % _URL_ROOT)
+    # def web_login(self, *args, **kw):
+    #     return super(SportClubManager, self).web_login(*args, **kw)
+    # @http.route('%s/signup/' % _URL_ROOT, type='http', auth='public', website=True, sitemap=False)
+    # def web_auth_signup(self, *args, **kw):
+    #     return super(SportClubManager, self).web_auth_signup(*args, **kw)
+    # @http.route('%s/reset_password' % _URL_ROOT, type='http', auth='public', website=True, sitemap=False)
+    # def web_auth_reset_password(self, *args, **kw):
+    #     return super(SportClubManager, self).web_auth_reset_password(*args, **kw)
 
-    @http.route('/web/reset_password', type='http', auth='public', website=True, sitemap=False)
-    def web_auth_reset_password(self, *args, **kw):
-        return super(Sport, self).web_auth_reset_password(*args, **kw)
+    @http.route('%s/request_affiliation' % _URL_ROOT, auth='user', website=True)
+    def request_affiliation(self):
+        return 'TODO...'
 
-    # @http.route('/sport/session_list/', auth='public', website=True)
+
+    @http.route('%s/<model("res.users"):member>/' % _URL_ROOT, auth='public', website=True)
+    def member(self, member):
+        return http.request.render('sport_club_manager.website_member_info', {
+            'player': member
+        })
+
+    @http.route()
+    def info_staff(self):
+        pass  # TODO Affichier secrétaire, président et trésorier + nbr de récréants, etc
+
+    # @http.route('%s/session_ % _URL_ROOTlist/', auth='public', website=True)
     # def session_list(self, **kw):
     #     sessions = http.request.env['sport_club_manager.session'].search([])
     #     return http.request.render('sport_club_manager.session_list', {
     #         'session': sessions
     #     })
 
-    # @http.route('/sport/<int:id>/', auth='public', website=True)
+    # @http.route('%s/<int:id> % _URL_ROOT/', auth='public', website=True)
     # def teacher(self, id):
     #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
 
-    # @http.route('/sport/<model("sport_club_manager.session"):session>/', auth='public', website=True)
+    # @http.route('%s/<model(" % _URL_ROOTsport_club_manager.session"):session>/', auth='public', website=True)
     # def session(self, session):
     #     return http.request.render('sport_club_manager.session', {
     #         'session': session
