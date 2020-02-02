5875afef95b0a9498432db95065a053587869992
pierre <pal@odoo.com>
2020-02-02 23:56:22 +0100

    [IMP] interclubs: Add filter in interclub event kanban view, based on the interclub.

--- interclubs/models/calendar.py
+++ interclubs/models/calendar.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Meeting(models.Model):
@@ -9,3 +9,16 @@ class Meeting(models.Model):
 
     item_color = fields.Char('Item Color',
         help='Color of the item in the calendar view')
+
+
+class CalendarInterclubs(models.Model):
+    _name = 'calendar.interclubs'
+    _description = 'Calendar Interclubs'
+
+    user_id = fields.Many2one('res.users', 'Me', required=True, default=lambda self: self.env.user)
+    interclub_id = fields.Many2one('interclub', 'Interclub', required=True)
+    active = fields.Boolean('Active', default=True)
+
+    _sql_constraints = [
+        ('user_id_interclub_id_unique', 'UNIQUE(user_id,interclub_id)', 'An user cannot have twice the same interclub.')
+    ]
