d74eb9be944519ff4180c1d66fd1e65130349bb9
pierre <pal@odoo.com>
2020-10-11 01:18:59 +0200

    [REF][FIX] club: Extend fix of website menus hierarchy to `write` method
    The fix introduced in commit pieral85/sport_club_manager@d20c90e0 was only
    triggered when creating a new website menu (typically at the install of a module).
    As standard Odoo behaviour also breaks website menu hierarchy at the update,
    we need to add the same fix for the `write` method.
    Therefore, the fix itself has been refectored to be called in both methods.

--- club/models/website_menu.py
+++ club/models/website_menu.py
@@ -7,42 +7,55 @@ from odoo import api, models
 class WebsiteMenu(models.Model):
     _inherit = 'website.menu'
 
-    @api.model
-    def create(self, vals):
+    def _get_website_and_parent(self, website_id, parent_id):
         """ Get data more consistant regarding 'website_id' and 'parent_id' fields.
 
-        When installing module with some website menu records (coming from xml files),
+        When installing/updating module with some website menu records (coming from xml files),
         there were sometimes some strange behaviours:
          - If a record did not have any 'website_id' defined, it was set as a "root menu",
            even if a 'parent_id' menu was defined on the record.
          - It was possible to define a record on a website A, with a parent menu coming from
            website B. With this inherit, we try to associate the record with a similar
            parent menu coming from the same website A.
-        """
 
+        :return: updated website and menu parent
+        :rtype: dict(['website_id': int][, 'parent_id': int])
+        """
+        new_vals = {}
         if not self._context.get('install_mode'):
-            # When created manually (not at the module install), we trust the user
-            # and let him do what he wants
-            return super(WebsiteMenu, self).create(vals)
+            # When created/updated manually (not at the module install/update),
+            # we trust the user and let him do what he wants
+            return new_vals
 
         Menu = self.env['website.menu']
-        initial_website_id = vals.get('website_id') or self._context.get('website_id') or False
-        if initial_website_id:
-            if vals.get('parent_id'):
-                # Try to retrieve the parent menu identical to the one in 'vals'
-                # but coming from the same website then the one in 'vals'
-                initial_parent = Menu.browse(vals['parent_id'])
-                if initial_parent.website_id.id != initial_website_id:
+        website_id = website_id or self._context.get('website_id') or False
+        if website_id:
+            if parent_id:
+                # Try to retrieve the parent menu identical to `parent_id`
+                # but coming from the same website as the one of `website_id`
+                initial_parent = Menu.browse(parent_id)
+                if initial_parent.website_id.id != website_id:
                     new_parent = Menu.search([
-                        ('website_id', '=', initial_website_id),
+                        ('website_id', '=', website_id),
                         ('url', '=', initial_parent.url),
                     ], limit=1)
                     if new_parent and new_parent != initial_parent:
-                        vals['parent_id'] = new_parent.id
+                        new_vals['parent_id'] = new_parent.id
             else:
                 # When no parent menu is set, take the default one of the website
-                vals['parent_id'] = self.env['website'].browse(initial_website_id).menu_id.id
-        elif vals.get('parent_id'):
+                new_vals['parent_id'] = self.env['website'].browse(website_id).menu_id.id
+        elif parent_id:
             # When no website is set, assign the website of the parent menu
-            vals['website_id'] = Menu.browse(vals['parent_id']).website_id.id
+            new_vals['website_id'] = Menu.browse(parent_id).website_id.id
+        return new_vals
+
+    @api.model
+    def create(self, vals):
+        updated_vals = self._get_website_and_parent(vals.get('website_id'), vals.get('parent_id'))
+        vals.update(updated_vals)
         return super(WebsiteMenu, self).create(vals)
+
+    def write(self, vals):
+        updated_vals = self._get_website_and_parent(vals.get('website_id'), vals.get('parent_id'))
+        vals.update(updated_vals)
+        return super(WebsiteMenu, self).write(vals)
