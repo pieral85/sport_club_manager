58da6fc7d8ef56117a979ce9a6e13b7857d5e0b8
pierre <pal@odoo.com>
2022-03-27 15:49:44 +0200

    [MIG] interclubs: Modify interclub kanban as per v15 original view
    This includes the addition of the `interclub_checked` field which reflects
    the interclubs that are checked in the right panel of the kanban view.
    
    Other changes:
     - `hide_company` and `always_reload` context/option attributes have been
       removed as they did not have any effect.
       However, this should be investigated in the future.
     - Fix some typos

--- interclubs/models/calendar.py
+++ interclubs/models/calendar.py
@@ -18,7 +18,10 @@ class CalendarInterclubs(models.Model):
     user_id = fields.Many2one('res.users', 'Me', required=True, default=lambda self: self.env.user)
     interclub_id = fields.Many2one('interclub', 'Interclub', required=True)
     active = fields.Boolean('Active', default=True)
+    interclub_checked = fields.Boolean('Checked', default=True,
+        help="This field is used to know if the interclub is checked in the filter of the calendar view for "\
+        "the interclub_id.")
 
     _sql_constraints = [
-        ('user_id_interclub_id_unique', 'UNIQUE(user_id,interclub_id)', 'An user cannot have twice the same interclub.')
+        ('user_id_interclub_id_unique', 'UNIQUE(user_id, interclub_id)', 'A user cannot have the same interclub twice.')
     ]
