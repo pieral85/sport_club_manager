82b1a5d537da76b201fa60f54fd120e1f2b76ee3
pierre <pal@odoo.com>
2022-08-13 16:09:59 +0200

    Create Tool repo (should be in a separated development branch)

---
+++ tools/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "tools",
+
+    'summary': """
+        Provides a bunch of usefull tools.""",
+
+    'description': """
+    """,
+
+    'author': "pieral85@hotmail.com",
+    'website': "https://www.bcsaintleger.be",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['mail'],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
