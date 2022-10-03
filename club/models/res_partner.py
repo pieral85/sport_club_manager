b61b200ea8adf7de116e8f6a24f9188a6f20083f
pierre <pal@odoo.com>
2022-10-04 00:10:14 +0200

    [ADD] club: Implement membership(s) smart button on player form view

--- club/models/res_partner.py
+++ club/models/res_partner.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class ResPartner(models.Model):
@@ -18,3 +18,29 @@ class ResPartner(models.Model):
         This is usually useful for a minor child.')
     dependent_ids = fields.One2many('res.partner', 'responsible_id', string='Dependents')
     membership_ids = fields.One2many('membership', 'member_id', string='Memberships')
+    membership_count = fields.Integer('Count Memberships', compute='_compute_memberships')
+
+    def _compute_memberships(self):
+        for record in self:
+            record.membership_count = len(record.membership_ids)
+
+    def action_view_memberships(self):
+        self.ensure_one()
+        action = {
+            'name': _('Memberships'),
+            'res_model': 'membership',
+            'type': 'ir.actions.act_window',
+            'context': {'default_member_id': self.id},
+        }
+        membership_ids = self.membership_ids.ids
+        if len(membership_ids) == 1:
+            action.update({
+                'view_mode': 'form',
+                'res_id': membership_ids[0],
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', membership_ids)],
+            })
+        return action
